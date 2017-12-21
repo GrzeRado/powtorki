75f1ef9ba508256a12116f7bc136d80641260615
Grzegorz <rc36@wp.pl>
2017-12-21 14:48:14 +0100

    Foreach func

--- function/script.js
+++ function/script.js
@@ -19,9 +19,17 @@
    // return a+b
 //}
 
-var x = 2
-var blabla = function add(x) {
-    console.log(x)
-    if (x>0)
-    add (--x)
+//var x = 2
+///var blabla = function add(x) {
+   // console.log(x)
+   // if (x>0)
+  //  add (--x)
+///}
+
+
+var OurForeach = function (array, otherElement){
+    for (var = i; array.lenght; i++){
+        otherElement(array[i], i, array)
+    }
+
 }
